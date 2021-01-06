ee7646bf6c452bac7ef35ac88d9e36f7b400368f
Lai Wei <lai.wei@ibigroup.com>
2021-01-05 22:35:20 -0500

    create app

---
+++ ImageStyle/ImageStyle/ImageStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageStyleApp.swift
+//  ImageStyle
+//
+//  Created by sunnywei on 2021-01-05.
+//
+
+import SwiftUI
+
+@main
+struct ImageStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
